8564d53656a6f87bb90a8a5a9aea517c07bec265
Abdou majid <abdoumajid2003@gmail.com>
2024-07-09 12:08:33 +0000

    i correct the shop/script.js

--- shop/script.js
+++ shop/script.js
@@ -9,82 +9,72 @@ closeBtn.addEventListener('click', () => {
   contentBtn.style.width = '0';
 });
 
-
 const body = document.querySelector('body');
-const navOng = document.querySelectorAll('nav .onglet p a');
+const navLinks = document.querySelectorAll('nav .onglet p a');
 const moonSun = document.querySelector('#moon-sun');
 
 let isSun = false;
 
 moonSun.addEventListener('click', () => {
   body.classList.toggle('color-change');
-  
+
   if (isSun) {
     moonSun.innerHTML = "<i class='bx bxs-moon'></i>";
-    navOng.forEach(nav => { nav.style.color ='#1E123A'; }); // Fixed color value
+    navLinks.forEach(nav => { nav.style.color = '#1E123A'; }); // Fixed color value
     isSun = false;
   } else {
     moonSun.innerHTML = "<i class='bx bxs-sun'></i>";
-    navOng.forEach(nav => { nav.style.color ='#fff'; });
+    navLinks.forEach(nav => { nav.style.color = '#fff'; });
     isSun = true;
   }
 });
 
-
-
-const searchBar = document.querySelector('#search')
-const submitSearch = document.querySelector('#submit')
+const searchBar = document.querySelector('#search');
+const submitSearch = document.querySelector('#submit');
 
 function filterAndDisplayBrand(brand) {
   hideAllBrands(); // Hide all brands
   showBrand(brand); // Show the specified brand
 }
 
-function trye() {
+function filterSearch() {
   const searchText = searchBar.value.toLowerCase();
 
   if (searchText === 'cycling') {
-    filterAndDisplayBrand(cycling)
-  }
-  if (searchText === 'basket') {
-    filterAndDisplayBrand(basketBall)
-  }
-  if (searchText === 'rugby') {
-    filterAndDisplayBrand(rugby)
-  }
-  if (searchText === 'hokey') {
-    filterAndDisplayBrand(iceHokey)
-  }
- 
-  if (searchText === 'athletisme') {
-    filterAndDisplayBrand(athletisme)
+    filterAndDisplayBrand(cycling);
+  } else if (searchText === 'basket') {
+    filterAndDisplayBrand(basketBall);
+  } else if (searchText === 'rugby') {
+    filterAndDisplayBrand(rugby);
+  } else if (searchText === 'hokey') {
+    filterAndDisplayBrand(iceHokey);
+  } else if (searchText === 'athletisme') {
+    filterAndDisplayBrand(athletisme);
   }
 }
 
 searchBar.addEventListener('keyup', (evt) => {
   if (evt.key === 'Enter') {
-    trye();
+    filterSearch();
   }
 });
 
-submitSearch.addEventListerner('click', () => {
-  trye();
-})
+submitSearch.addEventListener('click', () => {
+  filterSearch();
+});
 
-const allBrand = document.querySelector('.all')
-const cyclingLetter = document.querySelector('.cycling-letter')
-const basketBallLetter = document.querySelector('.basket-ball-letter')
-const rugbyLetter = document.querySelector('.rugby-letter')
-const iceHokeyLetter = document.querySelector('.ice-hokey-letter')
-const athleLetter = document.querySelector('.athletisme-letter')
+const allBrand = document.querySelector('.all');
+const cyclingLetter = document.querySelector('.cycling-letter');
+const basketBallLetter = document.querySelector('.basket-ball-letter');
+const rugbyLetter = document.querySelector('.rugby-letter');
+const iceHokeyLetter = document.querySelector('.ice-hokey-letter');
+const athleLetter = document.querySelector('.athletisme-letter');
 
-
-const cycling = document.querySelectorAll('[data-cycling]')
-const basketBall = document.querySelectorAll('[data-basket-ball]')
-const rugby = document.querySelectorAll('[data-rugby]')
-const iceHokey = document.querySelectorAll('[data-ice-hokey]')
-const athletisme = document.querySelectorAll('[data-athle]')
-
+const cycling = document.querySelectorAll('[data-cycling]');
+const basketBall = document.querySelectorAll('[data-basket-ball]');
+const rugby = document.querySelectorAll('[data-rugby]');
+const iceHokey = document.querySelectorAll('[data-ice-hokey]');
+const athletisme = document.querySelectorAll('[data-athle]');
 
 function hideAllBrands() {
   cycling.forEach(item => { item.style.display = 'none'; });
@@ -99,36 +89,34 @@ function showBrand(brand) {
 }
 
 allBrand.addEventListener('click', () => {
-  showBrand(cycling)
-  showBrand(basketBall)
-  showBrand(rugby) 
-  showBrand(iceHokey)
-  showBrand(athletisme)
-})
-
-
+  showBrand(cycling);
+  showBrand(basketBall);
+  showBrand(rugby);
+  showBrand(iceHokey);
+  showBrand(athletisme);
+});
 
 cyclingLetter.addEventListener('click', () => {
-  hideAllBrands()
+  hideAllBrands();
   showBrand(cycling);
-})
+});
 
 basketBallLetter.addEventListener('click', () => {
-  hideAllBrands()
-  showBrand(basketBall)
-})
+  hideAllBrands();
+  showBrand(basketBall);
+});
 
 rugbyLetter.addEventListener('click', () => {
-  hideAllBrands()
+  hideAllBrands();
   showBrand(rugby);
-})
+});
 
 iceHokeyLetter.addEventListener('click', () => {
-  hideAllBrands()
+  hideAllBrands();
   showBrand(iceHokey);
-})
+});
 
 athleLetter.addEventListener('click', () => {
-  hideAllBrands()
+  hideAllBrands();
   showBrand(athletisme);
-})
+});
